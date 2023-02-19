5525616716ae8d6970194d20d467fc1077f28a8c
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-02-19 17:46:35 +0300

    Done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0 ; i< array.length ; i++){
+    const complement = target - array[i];
+
+    for(let j = i + 1 ; j < array.length ; j++){
+      if ( array[j] === complement )return true
+    }
+  }
+  return false;
 }
 
 /* 
@@ -8,6 +16,7 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  need function revive two arrgument 
 */
 
 /*
